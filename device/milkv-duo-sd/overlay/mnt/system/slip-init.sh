c1c144eed3ea5f67fd03e95fed89c3a1d4d01ea2
iamadahn <vendonism16@yandex.ru>
2024-12-22 21:21:44 +0300

    slip-init.sh: fix

--- device/milkv-duo-sd/overlay/mnt/system/slip-init.sh
+++ device/milkv-duo-sd/overlay/mnt/system/slip-init.sh
@@ -3,6 +3,10 @@
 ifconfig eth0 down
 
 slattach -L -p slip -s 115200 /dev/ttyS1&
+
+sleep 0.5
+
 ifconfig sl0 192.168.240.2 pointopoint 192.168.240.1 up mtu 1500
 route add default gw 192.168.240.1
-echo "nameserver 9.9.9.9" > /etc/resolv.conf
+touch /etc/resolv.conf.head
+echo "nameserver 9.9.9.9" > /etc/resolv.conf.head
