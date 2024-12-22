47fd2401d50245b2cfd95b01d73112ab4491ac93
iamadahn <vendonism16@yandex.ru>
2024-12-22 21:21:17 +0300

    duo-init.sh: UART1 pins config

--- device/milkv-duo-sd/overlay/mnt/system/duo-init.sh
+++ device/milkv-duo-sd/overlay/mnt/system/duo-init.sh
@@ -7,6 +7,10 @@ devmem 0x03001068 32 0x3
 # Set Pin-19(GP14) to GPIO
 duo-pinmux -w GP14/GP14 > /dev/null
 
+# Set Pin-0 and Pin-1 to UART1
+duo-pinmux -w GP0/UART1_TX
+duo-pinmux -w GP1/UART1_RX
+
 # Insmod PWM Module
 insmod /mnt/system/ko/cv180x_pwm.ko
 
